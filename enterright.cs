f3d302ac8b88db9385567c7bb49ec5a226fbdd11
aldawg96 <alduncan217@gmail.com>
2017-03-14 18:58:55 -0500

    added scripts for trigger boxes

---
+++ enterright.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightenter : MonoBehaviour
+{
+
+    public GameObject rtext;
+
+    private void OnTriggerEnter()
+    {
+        rtext.SetActive(true);
+    }
+
+
+
+}
