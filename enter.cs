3eb97613781d22b2ec1fb274f6d00f9b28042e21
aldawg96 <alduncan217@gmail.com>
2017-03-28 14:14:18 -0500

    Add files via upload

--- enter.cs
+++ enter.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class enter : MonoBehaviour {
 
 public GameObject ctext;
-
-    private void OnTriggerEnter()
+public GameObject atext;
+//public GUIText scoreText;
+//public int scoreValue;
+    private GvrController gvrcontroller;
+    public void OnTriggerEnter()
     {
+        atext.SetActive(false);
         ctext.SetActive(true);
     }
+    public void OnTriggerExit()
+    {
+      
+    atext.SetActive(true);
+    ctext.SetActive(false);
+   // gvrcontroller.AddScore(scoreValue);
+    }
 
-    
+  
 
+    /*  public void AddScore(int newScoreValue)
+      {
+          score += newScoreValue;
+          UpdateScore();
+      }
+
+      void UpdateScore()
+      {
+          scoreText.text = "Score: " + score;
+      }
+      */
 }
